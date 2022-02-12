0d3b240ac8c765e3eff541e097c91e218bf7c97d
Student <kishoiyancharity@gmail.com>
2022-02-12 15:49:08 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main (){
+    var name= "liam sam"
+    println(name)
+    var age = 22
+    println(age)
+    var phoneNumber= +254710315446
+    println(phoneNumber)
+    var weight:String = "80kg"
+    println(weight)
+    val iskenyan: Boolean= true
+    val isnotkenyan: Boolean= false
+    println(iskenyan)
+}
